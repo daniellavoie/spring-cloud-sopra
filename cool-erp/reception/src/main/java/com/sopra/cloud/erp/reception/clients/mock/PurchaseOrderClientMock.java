e54dd9350195246950dd6448b90434b0d85f3399
grlebon <lebongregory1@gmail.com>
2017-07-20 14:57:58 +0200

    update purchaseOrderClient mock

--- cool-erp/reception/src/main/java/com/sopra/cloud/erp/reception/clients/mock/PurchaseOrderClientMock.java
+++ cool-erp/reception/src/main/java/com/sopra/cloud/erp/reception/clients/mock/PurchaseOrderClientMock.java
@@ -2,13 +2,10 @@ package com.sopra.cloud.erp.reception.clients.mock;
 
 import com.sopra.cloud.erp.reception.clients.PurchaseOrderClient;
 
-/**
- * Created by grlebon on 20-07-17.
- */
 public class PurchaseOrderClientMock implements PurchaseOrderClient {
 
     @Override
-    public void notifyCommandReceived() {
+    public void notifyCommandReceived(long purchaseOrderId) {
 
     }
 }
