abe522fb59adb8d563fbfedbb6d35efc31151955
Olivier Ducarme <olivier.ducarme@soprabanking.com>
2017-07-20 16:36:18 +0200

    cloud stuff

--- cool-erp/reception/src/main/java/com/sopra/cloud/erp/reception/clients/InventoryClient.java
+++ cool-erp/reception/src/main/java/com/sopra/cloud/erp/reception/clients/InventoryClient.java
@@ -1,7 +1,15 @@
 package com.sopra.cloud.erp.reception.clients;
 
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient("inventory-service")
 public interface InventoryClient {
 
-    void incrementInventory(long productId);
+
+    @RequestMapping(method = RequestMethod.POST, path = "/inventory/{productId}?increment")
+    void incrementInventory(@PathVariable("productId") long productId);
 
 }
